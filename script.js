edbbe204f1d83bbf82d808812b988bb45d2c9380
guias12 <guilherme.p.assis@gmail.com>
2020-06-19 10:25:54 -0300

    added the toggle language function

---
+++ script.js
@@ -0,0 +1,32 @@
+const selectAll = (s) => document.querySelectorAll(s);
+let currentLanguage = 'english';
+let flagImages = selectAll('.flag');
+let englishElements = selectAll('.english');
+let portugueseElements = selectAll('.portuguese');
+flagImages = [...flagImages]
+englishElements = [...englishElements]
+portugueseElements = [...portugueseElements]
+
+console.log(flagImages)
+ 
+flagImages.map(flag => {
+    flag.addEventListener('click', () => {
+        if(currentLanguage == 'english') {
+            englishElements.map(englishElement => {
+                englishElement.classList.add('disabled');
+            })
+            portugueseElements.map(portugueseElement => {
+                portugueseElement.classList.remove('disabled')
+            })            
+            currentLanguage = 'portuguese'
+        } else if(currentLanguage == 'portuguese') {
+            englishElements.map(englishElement => {
+                englishElement.classList.remove('disabled');
+            })
+            portugueseElements.map(portugueseElement => {
+                portugueseElement.classList.add('disabled')
+            })            
+            currentLanguage = 'english'            
+        }
+    })
+})
